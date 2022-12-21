61a9b803a7ff62ad4d65bbc959cbcbd0fa46ea93
Manish Keshari <manish.keshari@worldline.com>
2022-12-21 19:52:00 +0530

    Pipes

--- projects/my-first-component/src/app/app.module.ts
+++ projects/my-first-component/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { PipesPipe } from './app/pipes.pipe';
+import { StarRatingPipe } from './pipes/star-rating.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    PipesPipe,
+    StarRatingPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
